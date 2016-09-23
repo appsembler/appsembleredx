e8b9e61ce14e2f04942dc0dbb8c15c0e42a7b52d
Bryan Wilson <bryanlandia@gmail.com>
2016-09-23 11:36:01 -0700

    Add all course mixin fields to certificate webview tmpl context

--- appsembleredx/monkeypatch.py
+++ appsembleredx/monkeypatch.py
@@ -4,6 +4,17 @@ from course_modes import models as course_modes_models
 from appsembleredx import app_settings
 from appsembleredx import mixins
 
+import logging
+
+# some trickery here to get around AppRegsitryNotReady error b/c of translation strings otherwise
+from django.utils import translation
+orig_ugettext = translation.ugettext
+translation.ugettext =  translation.ugettext_noop
+from certificates.views import webview
+translation.ugettext = orig_ugettext
+
+logger = logging.getLogger(__name__)
+
 
 def get_CourseDescriptor_mixins():
 	new_mixins = [mixins.XMLDefinitionChainingMixin, mixins.CertificatesExtensionMixin, ]
@@ -14,13 +25,18 @@ def get_CourseDescriptor_mixins():
 	return tuple(new_mixins)
 
 
+logger.warn('Monkeypatching course_module.CourseDescriptor to add Appsembler Mixins')
 orig_CourseDescriptor = course_module.CourseDescriptor
 CDbases = course_module.CourseDescriptor.__bases__
 course_module.CourseDescriptor.__bases__ = get_CourseDescriptor_mixins() + CDbases
 
-
+logger.warn('Monkeypatching course_modes_models.CourseMode.DEFAULT_MODE_SLUG and ...DEFAULT_MODE')
 orig_DEFAULT_MODE_SLUG = course_modes_models.CourseMode.DEFAULT_MODE_SLUG
 course_modes_models.CourseMode.DEFAULT_MODE_SLUG = app_settings.DEFAULT_COURSE_MODE_SLUG
 orig_DEFAULT_MODE = course_modes_models.CourseMode.DEFAULT_MODE
 course_modes_models.CourseMode.DEFAULT_MODE = app_settings.DEFAULT_COURSE_MODE
 
+logger.warn('Monkeypatching lms.djangoapps.certificates.views.webview._update_course_context to extend with Appsembler Mixin fields')
+orig__update_course_context = webview._update_course_context
+from appsembleredx import views
+webview._update_course_context = views._update_course_context
--- appsembleredx/views.py
+++ appsembleredx/views.py
@@ -3,6 +3,13 @@ import logging
 from django.contrib.auth.decorators import login_required
 
 from edxmako.shortcuts import render_to_response
+from xblock import fields
+
+from appsembleredx.monkeypatch import (
+	orig__update_course_context,
+	get_CourseDescriptor_mixins
+)
+
 
 log = logging.getLogger(__name__)
 
@@ -10,3 +17,19 @@ log = logging.getLogger(__name__)
 def index(request):
     context = {}
     return render_to_response('boilerplate/index.html', context)
+
+
+# CERTIFICATES-RELATED
+def _update_course_context(request, context, course, platform_name):
+	"""
+	Course-related context for certificate webview, extended
+	with Mixin fields
+	"""
+
+	orig__update_course_context(request, context, course, platform_name)
+
+	# add our course extension fields
+	course_mixins = get_CourseDescriptor_mixins()
+	for mixin in course_mixins:
+		for f in mixin.fields:
+			context[f] = getattr(course, f)
