1604976b7463418410c480db38a0ee7b496eebf7
Bryan Wilson <bryanlandia@gmail.com>
2016-09-29 09:23:18 -0700

    Read LinkedIn config from env, mgmt cmd to create Django config

---
+++ appsembleredx/management/commands/create_linkedin_config.py
@@ -0,0 +1,33 @@
+# run this to enable LinkedIn configuration on an Open edX install
+# called by Ansible playbook
+
+from django.core.management.base import BaseCommand, CommandError
+
+from student import models
+
+from appsembleredx import app_settings
+
+
+class Command(BaseCommand):
+    help = """Creates a LinkedInAddToProfileConfiguration record to enable 
+    certificate sharing on LinkedIn   
+    """
+
+    def handle(self, *args, **options):
+        
+        def stdout(msg, style=self.style.NOTICE):
+            self.stdout.write(style(msg))
+        
+        company_id = app_settings.LINKEDIN_ADDTOPROFILE_COMPANY_ID
+        if not company_id:
+        	raise CommandError("You must specify a value for APPSEMBLER_FEATURES['LINKEDIN_ADDTOPROFILE_COMPANY_ID'] in your env.json file")
+
+        try:
+        	
+            enable =  models.LinkedInAddToProfileConfiguration(company_identifier=company_id, enabled=True)
+            enable.save()
+        except:  # pylint: disable=broad-except
+            stdout("Couldn't enable a LinkedIn Add to Profile configuration", style=self.style.ERROR)
+            raise CommandError("Couldn't enable a LinkedIn Add to Profile configuration")
+        
+        stdout('Enabled a LinkedIn Add to Profile configuration')
