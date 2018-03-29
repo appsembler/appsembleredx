4d17289bf1490e1555da767425b4c114d68434b2
Bryan Wilson <bryanlandia@gmail.com>
2018-03-29 16:24:49 -0700

    Exercise cert/badge related signal handlers other than cert creation

---
+++ appsembleredx/tests/test_signals.py
@@ -0,0 +1,161 @@
+""" Unit tests for signal handlers on course publication activity
+"""
+from functools import wraps
+import mock
+
+from django.core.exceptions import ObjectDoesNotExist
+
+from certificates.models import (
+    CertificateGenerationConfiguration, CertificateGenerationCourseSetting)
+from course_modes.models import CourseMode
+from openedx.core.djangoapps.self_paced.models import SelfPacedConfiguration
+from xmodule.modulestore.django import modulestore
+from xmodule.modulestore.tests.factories import CourseFactory
+from xmodule.modulestore.tests.django_utils import ModuleStoreTestCase
+
+from appsembleredx import signals
+
+
+class CertsSettingsSignalsTest(ModuleStoreTestCase):
+    """ Tests for signal handlers changing cert-related settings on course
+        publish or pre-publish.  None of the handlers should do anything 
+        if certificates feature is not enabled.
+    """
+
+    def setUp(self):
+        super(CertsSettingsSignalsTest, self).setUp()
+        # Enable certificates feature overall
+        CertificateGenerationConfiguration.objects.create(enabled=True)
+        # allow self-paced courses
+        SelfPacedConfiguration(enabled=True).save()
+        self.course = CourseFactory.create(self_paced=True)
+        self.store = modulestore()
+        self.mock_app_settings = mock.Mock()
+
+    def certs_feature_enabled(func):
+        @wraps(func)
+        @mock.patch.dict('appsembleredx.signals.settings.FEATURES', {'CERTIFICATES_ENABLED': True})
+        def with_certs_enabled(*args, **kwargs):
+            # reload to re-evaluate the decorated methods with new setting
+            reload(signals)
+            return func(*args, **kwargs)
+
+        return with_certs_enabled
+
+    @mock.patch.dict('appsembleredx.signals.settings.FEATURES', {'CERTIFICATES_ENABLED': False})
+    def test_signal_handlers_disabler_decorator(self):
+        """ Verify decorator works to return a noop function if CERTIFICATES_ENABLED is False
+        """
+        mock_decorated = mock.Mock()
+        mock_decorated.__name__ = 'mocked'  # needed for functools.wrap
+        ret_func = signals.disable_if_certs_feature_off(mock_decorated)
+        self.assertNotEqual(ret_func, mock_decorated)
+        self.assertTrue('noop_handler' in ret_func.__name__)
+
+    @certs_feature_enabled
+    def test_default_course_mode_changed_on_publish(self):
+        """ Verify the signal handler sets new default course mode
+        """
+        # there should be no coursemode object for the course
+        self.assertRaises(ObjectDoesNotExist, CourseMode.objects.get, **{"course_id": self.course.id})
+        # publish signal creates a new coursemode object
+        signals._default_mode_on_course_publish('store', self.course.id)
+        newmode = CourseMode.objects.get(course_id=self.course.id)
+        self.assertEqual(newmode.mode_slug, 'honor' )
+        # second publish doesn't create a new coursemode object
+        signals._default_mode_on_course_publish('store', self.course.id)
+        modes = CourseMode.objects.filter(course_id=self.course.id)
+        self.assertEqual(len(modes), 1)
+    
+    @certs_feature_enabled
+    def test_cert_related_advanced_settings_as_expected_by_default(self):
+        """ Verify that cert-related advanced course settings are what
+            we think by default
+        """
+        self.assertEqual(self.course.certificates_display_behavior, 'end')
+        self.assertFalse(self.course.certificates_show_before_end)
+        self.assertFalse(self.course.cert_html_view_enabled)
+
+    @certs_feature_enabled
+    def test_cert_related_advanced_settings_features(self):
+        """ Verify changes to cert-related advanced settings if we 
+            enable the feature or don't enable it
+        """
+        signals._change_cert_defaults_on_pre_publish('store', self.course.id)
+        course = self.store.get_course(self.course.id)
+        self.assertEqual(course.certificates_display_behavior, 'end')
+        self.assertFalse(course.certificates_show_before_end)
+        self.assertFalse(course.cert_html_view_enabled)
+
+        self.mock_app_settings.USE_OPEN_ENDED_CERTS_DEFAULTS = True
+        with mock.patch('appsembleredx.signals.app_settings', new=self.mock_app_settings):
+            signals._change_cert_defaults_on_pre_publish('store', self.course.id)
+            course = self.store.get_course(self.course.id)
+            self.assertEqual(course.certificates_display_behavior, 'early_with_info')
+            self.assertTrue(course.certificates_show_before_end)
+            self.assertTrue(course.cert_html_view_enabled)
+
+    def test_badges_remain_disabled_after_pre_publish_with_feature_off(self):
+        """ Verify that badges-related advanced course settings are not changed
+            when we use related feature settings
+        """        
+        with mock.patch.dict('appsembleredx.signals.settings.FEATURES', {'ENABLE_OPENBADGES': False}):
+            signals._change_badges_setting_on_pre_publish('store', self.course.id)
+            course = self.store.get_course(self.course.id)
+            self.assertFalse(course.issue_badges)
+
+    def test_badges_remain_disabled_after_pre_publish_with_feature_on_but_course_completion_badges_off(self):
+        """ Make sure we can enable to open badges feature overall but keep course completion
+            badges off using our setting
+        """    
+        self.mock_app_settings.DISABLE_COURSE_COMPLETION_BADGES = True    
+        with mock.patch.dict('appsembleredx.signals.settings.FEATURES', {'ENABLE_OPENBADGES': True}):
+            with mock.patch('appsembleredx.signals.app_settings', new=self.mock_app_settings):
+                signals._change_badges_setting_on_pre_publish('store', self.course.id)
+                course = self.store.get_course(self.course.id)
+                self.assertFalse(course.issue_badges)
+
+    @certs_feature_enabled
+    def test_enable_self_generated_certs_on_publish_for_self_paced(self):
+        """ Verify that self-generated certs are not enabled for self-paced courses
+            if it is explicitly disabled by setting
+        """
+        course = self.store.get_course(self.course.id)
+        # this should fail when we move to Ginkgo
+        self.assertFalse(CertificateGenerationCourseSetting.is_enabled_for_course(self.course.id))
+        signals.enable_self_generated_certs('store', self.course.id)
+        course = self.store.get_course(self.course.id)
+        self.assertTrue(CertificateGenerationCourseSetting.is_enabled_for_course(course.id))
+
+    @certs_feature_enabled
+    def test_dont_enable_self_generated_certs_on_publish_for_self_paced_when_disabled_by_setting(self):
+        """ Verify that self-generated certs are not enabled for self-paced courses
+            if it is explicitly disabled by setting
+        """
+        self.mock_app_settings.DISABLE_SELF_GENERATED_CERTS_FOR_SELF_PACED = True    
+        with mock.patch('appsembleredx.signals.app_settings', new=self.mock_app_settings):
+            signals.enable_self_generated_certs('store', self.course.id)
+            course = self.store.get_course(self.course.id)
+            self.assertFalse(CertificateGenerationCourseSetting.is_enabled_for_course(course.id))
+
+    @certs_feature_enabled
+    def test_dont_enable_self_generated_certs_on_publish_for_instructor_paced(self):
+        """ Verify that self-generated certs are not enabled for self-paced courses
+            if it is explicitly disabled by setting
+        """
+        self.course = CourseFactory.create(self_paced=False)
+        signals.enable_self_generated_certs('store', self.course.id)
+        course = self.store.get_course(self.course.id)
+        self.assertFalse(CertificateGenerationCourseSetting.is_enabled_for_course(course.id))
+
+    @certs_feature_enabled
+    def test_enable_self_generated_certs_on_publish_for_instructor_paced_if_always_enabled_by_setting(self):
+        """ Verify that self-generated certs are not enabled for self-paced courses
+            if it is explicitly disabled by setting
+        """
+        self.mock_app_settings.ALWAYS_ENABLE_SELF_GENERATED_CERTS = True    
+        with mock.patch('appsembleredx.signals.app_settings', new=self.mock_app_settings):
+            self.course = CourseFactory.create(self_paced=False)
+            signals.enable_self_generated_certs('store', self.course.id)
+            course = self.store.get_course(self.course.id)
+            self.assertTrue(CertificateGenerationCourseSetting.is_enabled_for_course(course.id))
